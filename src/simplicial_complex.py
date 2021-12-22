8721c9898179d45bb533f04c5feb2a185760a86e
Jeremy Wayland <jwayland@chapman.edu>
2021-12-22 12:25:48 -0800

    Reverted simplicial_complex.py to Daniel's last commit
    Accidentally edited this file and pushed it through.

--- src/simplicial_complex.py
+++ src/simplicial_complex.py
@@ -1,123 +1,117 @@
-def feature_selection(data,max_dim,min_persistence):
-    """This function will compute the persistence barcode of a point cloud using Ripser, and plot the diagram.
+import numpy as np
+import pandas as pd
+import networkx as nx
+import warnings
+from itertools import combinations
 
-This function uses scikit-tda's ripser.py: a lean persistent homology package for Python.
 
-Note:
-    max_dim: specifies the maximum dimension of homology to be computed
-    data: txt file or numpy array
-    
-     This will be done by selecting the betti numbers that persist longer than the threshold
-        Then radii can be selected that preserve homology for each successive dimension. 
+class SimplicialComplex:
     """
-    if type(data)==str:
-        with open(data) as f:
-            data = np.loadtxt(f, delimiter=delim)
+    This class generates a simplicial complex for a given set of data
+    and a given radius to determine adjacency between points.
+    After initialization, points may be added or removed from the complex.
+    The class can return, for a given point, what simplices the point belongs to
+    within the complex, and the distance, area, or generalized volume between points
+    represented by that simplex. The point need not already be a member of the complex.
+    The class can return, for a given point, the nearest k neighbors where:
+            All points are considered vertices on a weighted undirected graph
+                determined by the 1-simplices of the simplicial complex. The weights
+                will correspond to the distance between the connected vertices.
+            The neighbors of a point p are all points reachable from p by transversing
+                the edges of the graph.
+            Distance between two points (p,q) is determined by the shortest path along
+                the edges of the graph that connects p and q. The distance will be
+                the sum of the weights along this path. If no path exists between
+                p and q, then the distance between them will be undefined.
+    """
+    def __init__(self,
+                radius,
+                graph_distance_metric=None,
+                weight_distance_metric=None,
+                vertices=None,
+                delim=','):
+        ####CLASS INSTANCE VARIABLES
+        self.radius = radius
+        self.graph_distance_metric = graph_distance_metric
+        self.weight_distance_metric = weight_distance_metric
+        self.complex = nx.Graph()
+        self.node_shape = None
 
-    #Generate Persistence Diagram for Point Cloud and Visualize
-    dgms = ripser(data,maxdim=max_dim)['dgms']
-    plot_diagrams(dgms, show=True)
+        #define euclidean distance
+        def euclidean(x,y):
+            return np.linalg.norm(x-y)
+        #If either distance metric is set to None, default the metric to
+        #euclidean distance
+        if self.graph_distance_metric is None:
+            self.graph_distance_metric = euclidean
+        if self.weight_distance_metric is None:
+            self.weight_distance_metric = euclidean
+        #build simplicial complex if vertices are provided
+        if vertices is not None:
+            self.build_complex(vertices, delim)
 
-    
-    #Convert to Numpy
-    barcodes = np.array(dgms,dtype=object)
-    features = dict()
+    def build_complex(self, vertices, delim=None):
+        #import set of points from file, np array or pandas dataframe
+        points = self.import_vertices(vertices, delim)
+        #add points
+        for p in points:
+            self.add_point(p)
 
-    for dim in range(max_dim,0,-1):
-        barcode = barcodes[dim]
-        persistent_features = []
+    def add_point(self, point):
+        point = np.array(point)
+        assert self.node_shape is None or self.node_shape == point.shape,\
+            f'New point must be of the same shape as data already in simplex. '\
+            f'Expected {self.node_shape}, got {point.shape}'
+        #set shape of simplex data if this is the first point added to simplex
+        if self.node_shape is None:
+            self.node_shape = point.shape
+        # convert point to hashable tuple
+        point_id = tuple(point)
+        # do nothing if point already exists in complex
+        if self.complex.has_node(point_id):
+            return
+        self.complex.add_node(point_id)
+        distance_vector = self.get_distance_vector(point)
+        for graph_distance, weight_distance, node in distance_vector:
+            if graph_distance <= self.radius and graph_distance != 0:
+                # convert neighbor to hashable tuple
+                neighbor_id = tuple(node)
+                self.complex.add_edge(point_id,
+                                      neighbor_id,
+                                      weight=weight_distance)
 
-        if len(features)>0:
-            for interval in barcode:
-                persistence = abs(interval[1] - interval[0]) #Death of feature - Birth of Feature
-                #Save features that persist longer than min_persistence
-                if persistence > min_persistence:
-                    persistent_features.append(interval)
-        
-        #Ensure intervals are sorted by their lower bounds
-        persistent_features.sort(key=lambda x:x[0])
-        features[dim] = persistent_features
-        
-    return features
-            
+    def remove_point(self, point):
+        point_id = tuple(point)
+        if self.complex.has_node(point_id):
+            self.complex.remove_node(point_id)
 
-            
-def interval_intersection(array:list,flag=False):
-    if len(array) == 1:
-        return array[0],flag
-    else:
-        #Sort by lower bound
-        array.sort(key=lambda x:x[0])
-        
-        A = array[0]
-        B = array[1]
-       
-        #Check for intersection?
-        if min(B) <= max(A):
-            array[1] = np.array([min(B),min(max(A),max(B))]) 
-            print(f'Intersection, new interval: {array[1]}')
-            flag=True
+    def has_point(self, point):
+        point_id = tuple(point)
+        return self.complex.has_node(point_id)
+
+    def get_distance_vector(self, point):
+        '''
+        Returns list of tuples (graph_distance, weight_distance, node) of
+        the graph distance and weight distance between the point parameter
+        and all nodes (corresponding to the third tuple element) in the complex.
+        '''
+        graph_distance = \
+        [self.graph_distance_metric(point,np.array(x)) for x in self.complex.nodes]
+        if self.graph_distance_metric == self.weight_distance_metric:
+            weight_distance = graph_distance
         else:
-            print('No intersection')
-            if flag:
-                #If there has been previous intersection then have this interval propapgate
-                array[1] = A
-        #Recursive Call
-        return interval_intersection(array[1:],flag)
-        
+            weight_distance = \
+                [self.weight_distance_metric(point,np.array(x)) for x in self.complex.nodes]
+        distances = \
+            zip(graph_distance, weight_distance, list(self.complex.nodes))
+        return list(distances)
 
-def candidate_intervals(filtered_pd:dict):
-    #Just for dim 2 since we are only concerned with cycles
-    dim = max(filtered_pd.keys())
-    print(dim)
-    #Get Max Nontrivial Dimension Recursively
-    if len(filtered_pd[dim]) ==0 : #Trivial Barcode
-        filtered_pd.pop(dim)
-        print(f'Trimmed dictionary: {filtered_pd}')
-        if len(filtered_pd) == 0:
-            print('No candidate intervals found. Try increasing minimum persistence threshold')
-            return None
-        return candidate_intervals(filtered_pd)
-
-    #make sure intervals are sorted  
-    print(f'Im printing my persistence dictionary: {filtered_pd}')
-
-    cap,flag = interval_intersection(filtered_pd[dim])
-    print(f"The intersection is: {cap}")
-    #Have dimensions collapsed?
-    if dim == 1:
-        print('Dimension is 1')
-        #Is there nontrivial intersection
-        print(filtered_pd[dim][-1])
-        if flag:
-            print('About to return, should not recurse farther')
-            return cap
-            #returns list of all non-trivial homology intervals
-            
+    def import_vertices(self, vertices, delim=','):
+        assert type(vertices) in [str, list, np.ndarray, pd.DataFrame],\
+            'Vertices input of incorrect type, must be list of numeric tuples,\
+            filepath string, Pandas Dataframe, or Numpy ndarray'
+        if type(vertices)==str:
+            with open(vertices) as f:
+                return np.loadtxt(f, delimiter=delim)
         else:
-            return filtered_pd[dim]
-
-    #dim 2 or greater
-    if flag:
-        print('No intersection confirmed, should procceed to next dimension')
-        #Proceed element wise down to next dimension
-            #Add all intervals to lower dimension
-        for interval in filtered_pd[dim]:
-            filtered_pd[dim-1].append(interval)
-    else:
-        filtered_pd[dim-1].append(cap)
-    #Remove current dimension and proceed to next, recursively
-    filtered_pd.pop(dim)
-    print(f'Trimmed pd: {filtered_pd}')
-
-    return candidate_intervals(filtered_pd)
-
-
-
-def select_radii(data,max_dim,min_persistence):
-    
-    features = feature_selection(data,max_dim,min_persistence)
-    candidates = candidate_intervals(features)
-    
-    #Take the median of each candidate interval
-    return [np.mean(x) for x in candidates]
+            return np.array(vertices)
